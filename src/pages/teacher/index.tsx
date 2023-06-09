2824c791b219f6d0723978ce12b35459c0096091
Le Anh Tuan <atctw.it@gmail.com>
2023-06-09 18:03:14 +0700

    add: fetch data departments for teacher page

--- src/pages/teacher/index.tsx
+++ src/pages/teacher/index.tsx
@@ -11,74 +11,25 @@ import {
   EllipsisHorizontalCircleIcon,
 } from "@heroicons/react/24/outline";
 import { Listbox, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { classNames } from "@/utils/class-name-util";
-
-const people = [
-  {
-    id: 1,
-    name: "Wade Cooper",
-    avatar:
-      "https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    id: 2,
-    name: "Arlene Mccoy",
-    avatar:
-      "https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    id: 3,
-    name: "Devon Webb",
-    avatar:
-      "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80",
-  },
-  {
-    id: 4,
-    name: "Tom Cook",
-    avatar:
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    id: 5,
-    name: "Tanya Fox",
-    avatar:
-      "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    id: 6,
-    name: "Hellen Schmidt",
-    avatar:
-      "https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    id: 7,
-    name: "Caroline Schultz",
-    avatar:
-      "https://images.unsplash.com/photo-1568409938619-12e139227838?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    id: 8,
-    name: "Mason Heaney",
-    avatar:
-      "https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    id: 9,
-    name: "Claudie Smitham",
-    avatar:
-      "https://images.unsplash.com/photo-1584486520270-19eca1efcce5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-  {
-    id: 10,
-    name: "Emil Schaefer",
-    avatar:
-      "https://images.unsplash.com/photo-1561505457-3bcad021f8ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  },
-];
+import { Department } from "@/types/department.type";
+import axios from "axios";
+import { ATTENDANCE_API_DOMAIN } from "@/constants/axios-constant";
 
 const TeacherPage = () => {
-  const [selected, setSelected] = useState(people[3]);
+  const [departments, setDepartments] = useState<Department[]>([]);
+  const [selectedDepartment, setSelectedDepartment] =
+    useState<Department | null>(null);
+
+  useEffect(() => {
+    const fetchListOfDepartments = async () => {
+      const { data } = await axios.get(`${ATTENDANCE_API_DOMAIN}/department`);
+      setDepartments(data);
+    };
+
+    fetchListOfDepartments();
+  }, []);
 
   return (
     <Layout>
@@ -97,13 +48,18 @@ const TeacherPage = () => {
             <div className="py-4 md:py-7 px-4 md:px-8 xl:px-10">
               <div className="sm:flex items-center justify-between">
                 <div className="flex items-center">
-                  <Listbox value={selected} onChange={setSelected}>
+                  <Listbox
+                    value={selectedDepartment}
+                    onChange={setSelectedDepartment}
+                  >
                     {({ open }) => (
                       <div className="w-fit">
                         <Listbox.Button className="relative w-64 cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
                           <span className="flex items-center">
                             <span className="block truncate">
-                              {selected.name}
+                              {!selectedDepartment
+                                ? "All departments"
+                                : selectedDepartment.department_name}
                             </span>
                           </span>
                           <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
@@ -122,9 +78,35 @@ const TeacherPage = () => {
                           leaveTo="opacity-0"
                         >
                           <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-fit overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                            {people.map((person) => (
+                            <Listbox.Option
+                              className={({ active }) =>
+                                classNames(
+                                  active
+                                    ? "bg-indigo-600 text-white"
+                                    : "text-gray-900",
+                                  "relative cursor-default select-none py-2 pl-3 pr-9"
+                                )
+                              }
+                              value={null}
+                            >
+                              {({ selected, active }) => (
+                                <div className="flex items-center">
+                                  <span
+                                    className={classNames(
+                                      selected
+                                        ? "font-semibold"
+                                        : "font-normal",
+                                      "block truncate"
+                                    )}
+                                  >
+                                    All departments
+                                  </span>
+                                </div>
+                              )}
+                            </Listbox.Option>
+                            {departments.map((department) => (
                               <Listbox.Option
-                                key={person.id}
+                                key={department.id}
                                 className={({ active }) =>
                                   classNames(
                                     active
@@ -133,7 +115,7 @@ const TeacherPage = () => {
                                     "relative cursor-default select-none py-2 pl-3 pr-9"
                                   )
                                 }
-                                value={person}
+                                value={department}
                               >
                                 {({ selected, active }) => (
                                   <div className="flex items-center">
@@ -145,7 +127,7 @@ const TeacherPage = () => {
                                         "block truncate"
                                       )}
                                     >
-                                      {person.name}
+                                      {department.department_name}
                                     </span>
                                   </div>
                                 )}
